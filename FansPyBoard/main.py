f753a2a46a9c290349f377825d2c719f985839b9
philippe <philippe@pvico.net>
2016-08-15 22:53:21 +0200

    Declared fans tach pins

--- FansPyBoard/main.py
+++ FansPyBoard/main.py
@@ -6,6 +6,21 @@ BOTTOM_RAD_TOP_FANS_PWM_PIN = Pin.board.X3
 BOTTOM_RAD_BOTTOM_FANS_PWM_PIN = Pin.board.X4
 CPU_IN_WATER_TEMP_ADC_PIN = Pin.board.X19
 
+BOTOM_RAD_BOTTOM_FAN1_TACH_PIN = Pin.board.Y1
+BOTOM_RAD_BOTTOM_FAN2_TACH_PIN = Pin.board.Y2
+BOTOM_RAD_BOTTOM_FAN3_TACH_PIN = Pin.board.Y3
+BOTOM_RAD_BOTTOM_FAN4_TACH_PIN = Pin.board.Y4
+
+BOTTOM_RAD_TOP_FAN1_TACH_PIN = Pin.board.Y5
+BOTTOM_RAD_TOP_FAN2_TACH_PIN = Pin.board.Y6
+BOTTOM_RAD_TOP_FAN3_TACH_PIN = Pin.board.Y7
+BOTTOM_RAD_TOP_FAN4_TACH_PIN = Pin.board.Y8
+
+TOP_RAD_FAN1_TACH_PIN = Pin.board.X9
+TOP_RAD_FAN2_TACH_PIN = Pin.board.X10
+TOP_RAD_FAN3_TACH_PIN = Pin.board.X11
+TOP_RAD_FAN4_TACH_PIN = Pin.board.X12
+
 TEMPERATURE_READING_ISR_TIMER = 9
 FANS_PWM_TIMER = 2
 TOP_RAD_FANS_PWM_CHANNEL = 4    # PyBoard Lite only !
@@ -14,7 +29,7 @@ BOTTOM_RAD_BOTTOM_FANS_PWM_CHANNEL = 2    # PyBoard Lite only !
 
 MINIMUM_RPM_DUTY_TIME = 10
 
-TEMPERATURE_SENSOR_DIVIDER_RESISTANCE = 2200    # we have a 2.2k from adc pin to ground
+TEMPERATURE_SENSOR_DIVIDER_RESISTANCE = 2200    # we have a 2k2 from adc pin to ground
 
 NUMBER_OF_TEMPERATURE_READINGS_PER_SECOND = 10
 NUMBER_OF_READINGS_ARRAY_SIZE = NUMBER_OF_TEMPERATURE_READINGS_PER_SECOND * 5
@@ -22,7 +37,7 @@ NUMBER_OF_READINGS_ARRAY_SIZE = NUMBER_OF_TEMPERATURE_READINGS_PER_SECOND * 5
 
 def readTemperatureInterruptServiceRoutine(timer):
     global controller
-    controller.probeCpuInWaterTemperature()
+    controller._probeCpuInWaterTemperature()
 
 class Controller:
     def __init__(self):
@@ -37,25 +52,22 @@ class Controller:
 
         self.setAllFansMinSpeed()
 
-        # self._pinPwmTopRadFans = Pin(TOP_RAD_FANS_PWM_PIN, Pin.OUT_PP)
-        # self._pinPwmBottomRadTopFans = Pin(BOTTOM_RAD_TOP_FANS_PWM_PIN, Pin.OUT_PP)
-        # self._pinPwmBottomRadBottomFans = Pin(BOTTOM_RAD_BOTTOM_FANS_PWM_PIN, Pin.OUT_PP)
-
         self._adcCpuInWaterTemp = ADC(CPU_IN_WATER_TEMP_ADC_PIN)
         # 725 = reading for 25 deg. C
         self._cpuInWaterAdcReadings = [725 for c in range(NUMBER_OF_READINGS_ARRAY_SIZE)]
         self._timerTemperatureReadingIsr.callback(readTemperatureInterruptServiceRoutine)
 
 
+    def _probeCpuInWaterTemperature(self):   # To be called only by ISR
+        self._cpuInWaterAdcReadings[self.temperatureReadingCounter] = self._adcCpuInWaterTemp.read()
+        self.temperatureReadingCounter += 1
+        self.temperatureReadingCounter = self.temperatureReadingCounter % NUMBER_OF_READINGS_ARRAY_SIZE
+
+
     def setAllFansMinSpeed(self):
         self._channelTopRadPwm.pulse_width_percent(MINIMUM_RPM_DUTY_TIME)
         self._channelBottomRadTopFansPwm.pulse_width_percent(MINIMUM_RPM_DUTY_TIME)
         self._channelBottomRadBottomFansPwm.pulse_width_percent(MINIMUM_RPM_DUTY_TIME)
-
-    def probeCpuInWaterTemperature(self):   # performed within ISR
-        self._cpuInWaterAdcReadings[self.temperatureReadingCounter] = self._adcCpuInWaterTemp.read()
-        self.temperatureReadingCounter += 1
-        self.temperatureReadingCounter = self.temperatureReadingCounter % NUMBER_OF_READINGS_ARRAY_SIZE
 
 
     def cpuInWaterTemperature(self):
