48112f0497c441e49e568892da1bc99874e583b3
philippe <philippe@pvico.net>
2016-08-15 13:48:58 +0200

    Corrected bugs

--- FansPyBoard/main.py
+++ FansPyBoard/main.py
@@ -1,4 +1,4 @@
-import pyb
+import pyb, math
 from pyb import Pin, Timer, ADC
 
 TOP_RAD_FANS_PWM_PIN = Pin.board.X2
@@ -17,6 +17,7 @@ MINIMUM_RPM_DUTY_TIME = 10
 TEMPERATURE_SENSOR_DIVIDER_RESISTANCE = 2200    # we have a 2.2k from adc pin to ground
 
 NUMBER_OF_TEMPERATURE_READINGS_PER_SECOND = 10
+NUMBER_OF_READINGS_ARRAY_SIZE = NUMBER_OF_TEMPERATURE_READINGS_PER_SECOND * 5
 
 
 def readTemperatureInterruptServiceRoutine(timer):
@@ -26,6 +27,7 @@ def readTemperatureInterruptServiceRoutine(timer):
 class Controller:
     def __init__(self):
         self.temperatureReadingCounter = 0
+        self._cpuInTemperatureDataReady = False
         self._timerFansPwm = Timer(FANS_PWM_TIMER, freq=25000)
         self._timerTemperatureReadingIsr = Timer(TEMPERATURE_READING_ISR_TIMER, freq = NUMBER_OF_TEMPERATURE_READINGS_PER_SECOND)
 
@@ -40,7 +42,9 @@ class Controller:
         # self._pinPwmBottomRadBottomFans = Pin(BOTTOM_RAD_BOTTOM_FANS_PWM_PIN, Pin.OUT_PP)
 
         self._adcCpuInWaterTemp = ADC(CPU_IN_WATER_TEMP_ADC_PIN)
-        self._cpuInWaterAdcReadings = [725 for c in range(NUMBER_OF_TEMPERATURE_READINGS_PER_SECOND)]    # 725 = reading for 25 deg. C
+        # 725 = reading for 25 deg. C
+        self._cpuInWaterAdcReadings = [725 for c in range(NUMBER_OF_READINGS_ARRAY_SIZE)]
+        print(self._cpuInWaterAdcReadings)
         self._timerTemperatureReadingIsr.callback(readTemperatureInterruptServiceRoutine)
 
 
@@ -49,26 +53,28 @@ class Controller:
         self._channelBottomRadTopFansPwm.pulse_width_percent(MINIMUM_RPM_DUTY_TIME)
         self._channelBottomRadBottomFansPwm.pulse_width_percent(MINIMUM_RPM_DUTY_TIME)
 
-    def probeCpuInWaterTemperature(self):   # called bu ISR
+    def probeCpuInWaterTemperature(self):   # performed within ISR
         self._cpuInWaterAdcReadings[self.temperatureReadingCounter] = self._adcCpuInWaterTemp.read()
-        self.temperatureReadingCounter = (self.temperatureReadingCounter + 1) % NUMBER_OF_TEMPERATURE_READINGS_PER_SECOND
+        self.temperatureReadingCounter += 1
+        self.temperatureReadingCounter = self.temperatureReadingCounter % NUMBER_OF_READINGS_ARRAY_SIZE
+
 
     def cpuInWaterTemperature(self):
         irqState = pyb.disable_irq()    # critical section
-        averageAdcReading = sum(self._cpuInWaterAdcReadings) / (1.0 * NUMBER_OF_TEMPERATURE_READINGS_PER_SECOND)
+        averageAdcReading = sum(self._cpuInWaterAdcReadings) / (NUMBER_OF_READINGS_ARRAY_SIZE * 1.0)
         pyb.enable_irq(irqState)        # end of critical section
-
         sensorResistance = ((4095.0 / averageAdcReading) - 1.0) * TEMPERATURE_SENSOR_DIVIDER_RESISTANCE
         # From MatLab curve fitting, adding 0.2 for compensation with othe temp indicator
         return 0.2 + (-.0019 * sensorResistance * sensorResistance + 38.14 * sensorResistance + 43870) / (sensorResistance - 827)
 
 
-def mainLoop(controller):
+def mainLoop():
+    global controller
+    
     while True:
-        pyb.delay(1000)
+        pyb.delay(5000)
         print("%3.1f" % controller.cpuInWaterTemperature())
 
 
 controller = Controller()
-
-mainLoop(controller)
+mainLoop()
